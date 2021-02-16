dbe352eaa504b4e3e5a798d092505bc9cdeed0a1
Jérémy Robert <jeremy.rober@epitech.eu>
2021-02-16 17:43:21 +0100

    task5

--- Day2/main.go
+++ Day2/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "SofwareGoDay2/server"
+import (
+	"SofwareGoDay2/server"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
 
 func main() {
+	err := godotenv.Load("env.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	Port := os.Getenv("PORT")
 	i := server.NewServer()
-	i.Run(":8080")
+	i.Run(":" + Port)
 }
--- Day2/routes/route.go
+++ Day2/routes/route.go
@@ -4,16 +4,27 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
 )
 
 func world(c *gin.Context){
-	c.JSON(http.StatusOK, "world")
+	message := os.Getenv("HELLO_MESSAGE")
+	if message == "" {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+	c.String(http.StatusOK, "%s", message)
+}
+
+func health(c *gin.Context) {
+	c.Status(http.StatusOK)
 }
 
 func query(c *gin.Context){
 	Message := c.Query("message")
 	if Message == "" {
 		c.AbortWithStatus(http.StatusBadRequest)
+		return
 	}
 	c.String(http.StatusOK, "Query is %s", Message)
 }
@@ -22,6 +33,7 @@ func param(c *gin.Context){
 	Message := c.Param("message")
 	if Message == "" {
 		c.AbortWithStatus(http.StatusBadRequest)
+		return
 	}
 	c.String(http.StatusOK, "Param is :'%s'", Message)
 }
@@ -30,6 +42,7 @@ func body(c *gin.Context){
 	Message := c.PostForm("message")
 	if Message == "" {
 		c.AbortWithStatus(http.StatusBadRequest)
+		return
 	}
 	c.String(http.StatusOK, "Body value is '%s'", Message)
 }
@@ -38,6 +51,7 @@ func header(c *gin.Context){
 	Message := c.GetHeader("message")
 	if Message == "" {
 		c.AbortWithStatus(http.StatusBadRequest)
+		return
 	}
 	c.String(http.StatusOK, "Header value:'%s'", Message)
 }
@@ -45,15 +59,16 @@ func header(c *gin.Context){
 func cookie(c *gin.Context){
 	cookie, err := c.Cookie("message")
 
-	if err != nil {
-		cookie = "NotSet"
-		c.SetCookie("message", "test", 3600, "/", "localhost", false, true)
+	if err != nil || cookie == "" {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
 	}
 	fmt.Printf("Cookie value: %s \n", cookie)
 }
 
 func ApplyRoutes(r *gin.Engine) {
 	r.GET("/hello", world)
+	r.GET("/health", health)
 	r.GET("/repeat-my-query", query)
 	r.GET("/repeat-my-param/:message", param)
 	r.POST("/repeat-my-body", body)
